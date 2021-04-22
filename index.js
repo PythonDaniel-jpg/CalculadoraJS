e1380758eb6fac543fc4a4657ad14e382983558c
Daniel Pereira <>
2021-04-22 06:35:44 +0000

    Mejoras y adiciones.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-console.log("Bienvenido, introduzca un numero par la suma")
+console.log("Bienvenido a la calculadora de JS")
 
 
 
-operacion=prompt("Introduzca la operacion que desea realizar", "")
+operacion=prompt("Introduzca la operacion que desea realizar, si necesita ayuda introduzca ayuda", "")
 
 
 
@@ -12,7 +12,7 @@ if (operacion == "+" ) {
   suma1=prompt("Introduzca el primer numero", "")
   suma2=prompt("Introduzca el segundo numero", "")
   var suma3 = (parseInt (suma1) + parseInt (suma2));
-  console.log(suma3)
+  console.log("El resultado es:", suma3)
 };
 
 if (operacion == "-") {
@@ -21,7 +21,7 @@ if (operacion == "-") {
   resta1=prompt("Introduzca el primer numero", "")
   resta2=prompt("Introduzca el segundo numero", "")
   var resta3= (parseInt (resta1) - parseInt (resta2))
-  console.log(resta3)
+  console.log("El resultado es:", resta3)
 };
 
 if (operacion == "*") {
@@ -30,7 +30,7 @@ if (operacion == "*") {
   multi1=prompt("Introduzca el primer numero", "")
   multi2=prompt("Introduzca el segundo numero", "")
   var multi3= (parseInt (multi1) * parseInt (multi2))
-  console.log(multi3)
+  console.log("El resultado es:", multi3)
 };
 
 if (operacion == "/") {
@@ -39,5 +39,83 @@ if (operacion == "/") {
   divi1=prompt("Introduzca el primer numero", "")
   divi2=prompt("Introduzca el segundo numero", "")
   var divi3= (parseInt (divi1) / parseInt (divi2))
-  console.log(divi3)
+  console.log("El resultado es:", divi3)
 };
+
+if (operacion == "suma" ) {
+  var suma1;
+  var suma2;
+  suma1=prompt("Introduzca el primer numero", "")
+  suma2=prompt("Introduzca el segundo numero", "")
+  var suma3 = (parseInt (suma1) + parseInt (suma2));
+  console.log("El resultado es:", suma3)
+};
+
+if (operacion == "resta") {
+  var resta1;
+  var resta2;
+  resta1=prompt("Introduzca el primer numero", "")
+  resta2=prompt("Introduzca el segundo numero", "")
+  var resta3= (parseInt (resta1) - parseInt (resta2))
+  console.log("El resultado es:", resta3)
+};
+
+if (operacion == "multiplicacion") {
+  var multi1;
+  var multi2;
+  multi1=prompt("Introduzca el primer numero", "")
+  multi2=prompt("Introduzca el segundo numero", "")
+  var multi3= (parseInt (multi1) * parseInt (multi2))
+  console.log("El resultado es:", multi3)
+};
+
+if (operacion == "division") {
+  var divi1;
+  var divi2;
+  divi1=prompt("Introduzca el primer numero", "")
+  divi2=prompt("Introduzca el segundo numero", "")
+  var divi3= (parseInt (divi1) / parseInt (divi2))
+  console.log("El resultado es:", divi3)
+};
+
+if (operacion == "Suma" ) {
+  var suma1;
+  var suma2;
+  suma1=prompt("Introduzca el primer numero", "")
+  suma2=prompt("Introduzca el segundo numero", "")
+  var suma3 = (parseInt (suma1) + parseInt (suma2));
+  console.log("El resultado es:", suma3)
+};
+
+if (operacion == "Resta") {
+  var resta1;
+  var resta2;
+  resta1=prompt("Introduzca el primer numero", "")
+  resta2=prompt("Introduzca el segundo numero", "")
+  var resta3= (parseInt (resta1) - parseInt (resta2))
+  console.log("El resultado es:", resta3)
+};
+
+if (operacion == "Multiplicacion") {
+  var multi1;
+  var multi2;
+  multi1=prompt("Introduzca el primer numero", "")
+  multi2=prompt("Introduzca el segundo numero", "")
+  var multi3= (parseInt (multi1) * parseInt (multi2))
+  console.log("El resultado es:", multi3)
+};
+
+if (operacion == "Division") {
+  var divi1;
+  var divi2;
+  divi1=prompt("Introduzca el primer numero", "")
+  divi2=prompt("Introduzca el segundo numero", "")
+  var divi3= (parseInt (divi1) / parseInt (divi2))
+  console.log("El resultado es:", divi3)
+};
+
+
+if (operacion == "ayuda") {
+console.log("Para sumar introduzca + o suma, para restart introduzca - o resta, para dividir introduzca / o dividir, para multiplicar introduzca * o multiplicacion")
+
+}
